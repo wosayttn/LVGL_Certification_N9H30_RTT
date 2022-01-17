294f9ea2397342d7ca17e3d4344b6941bc65878a
Wayne Lin <wclin@nuvoton.com>
2022-01-17 20:47:42 +0800

    Exported project.

---
+++ cconfig.h
@@ -0,0 +1,18 @@
+#ifndef CCONFIG_H__
+#define CCONFIG_H__
+/* Automatically generated file; DO NOT EDIT. */
+/* compiler configure file for RT-Thread in GCC*/
+
+#define HAVE_NEWLIB_H 1
+#define LIBC_VERSION "newlib 2.5.0"
+
+#define HAVE_SYS_SIGNAL_H 1
+#define HAVE_SYS_SELECT_H 1
+#define HAVE_PTHREAD_H 1
+
+#define HAVE_FDSET 1
+#define HAVE_SIGACTION 1
+#define GCC_VERSION_STR "6.3.1 20170215 (release) [ARM/embedded-6-branch revision 245512]"
+#define STDC "2011"
+
+#endif
